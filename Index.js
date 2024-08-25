93b8e057b9d39deccaea4dd5a3ebe391fcf8a760
Yan <outyan720@gmail.com>
2024-08-25 09:11:18 -0300

    nv

--- Index.js
+++ Index.js
@@ -1,2 +1,12 @@
 console.log("Olá mundo!")
 console.log("Hello")
+
+var nome = "Roberto"
+
+const emailPessoa = "roberto@gmail.com"
+
+let telefonePessoa = "34988935149"
+
+let idade = prompt("Digite sua idade:")
+
+console.log(idade)
