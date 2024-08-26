b8ccc171276d8dca9207931c49e2c6ac1db2bfbd
Yan <outyan720@gmail.com>
2024-08-26 09:00:35 -0300

    nv

--- Index.js
+++ Index.js
@@ -19,3 +19,20 @@ let b = 5
 
 console.log(a>=b)
 
+let primeiroValor = 6
+let segundoValor = 3
+
+console.log(primeiroValor + segundoValor)
+console.log(primeiroValor - segundoValor)
+console.log(primeiroValor / segundoValor)
+console.log(primeiroValor * segundoValor)
+console.log(primeiroValor % segundoValor)
+
+// for(inicializador; condicao; incremento){
+//     declaracao
+// }
+
+for(let i = 0; i < 5; i++){
+    console.log(i)
+}
+
