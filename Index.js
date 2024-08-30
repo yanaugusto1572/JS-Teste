f82699f5238cff5cf830972ebb4fce9073a312dd
Yan <outyan720@gmail.com>
2024-08-30 10:10:47 -0300

    nv

--- Index.js
+++ Index.js
@@ -146,10 +146,10 @@
 
 let boletoPago = false
 
-    if(boletoPago === 'true'){
-        console.log('O boleto está pago')
-    }else{
-        console.log('O boleto não está pago')
-    }
+if(boletoPago === true){
+    console.log('O boleto está pago')
+}else{
+    console.log('O boleto não está pago')
+}
 
     
